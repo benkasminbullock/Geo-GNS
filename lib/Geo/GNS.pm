d5385a6f7cd8430141b8e6eec4271407ac91965f
Ben Bullock <benkasminbullock@gmail.com>
2012-06-08 10:48:22 +0900

    Initial commit of Geo-GNS

---
+++ lib/Geo/GNS.pm
@@ -0,0 +1,13 @@
+=head1 NAME
+
+Geo::GNS
+
+=cut
+package Geo::GNS;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
---
+++ t/Geo-GNS.t
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+use Test::More tests => 1;
+BEGIN { use_ok('Geo::GNS') };
+use Geo::GNS;
+
+# Local variables:
+# mode: perl
+# End:
