eec281465959184419db556a8bd55e3f2d4a1b24
Ben Bullock <benkasminbullock@gmail.com>
2017-07-11 11:24:18 +0900

    Put pod in its own file

--- lib/Geo/GNS/Parser.pm
+++ lib/Geo/GNS/Parser.pm
@@ -1,14 +1,9 @@
-=head1 NAME
-
-Geo::GNS::Parser - parse a GNS data file
-
-=cut
 package Geo::GNS::Parser;
-require Exporter;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw/$data_dir parse_file/;
 use warnings;
 use strict;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw/parse_file/;
 use Carp;
 our $VERSION = '0.01';
 
@@ -21,52 +16,6 @@ my @fields = qw/RC UFI UNI LAT LONG DMS_LAT DMS_LONG MGRS JOG FC DSG
 PC CC1 ADM1 POP ELEV CC2 NT LC SHORT_FORM GENERIC SORT_NAME_RO
 FULL_NAME_RO FULL_NAME_ND_RO SORT_NAME_RG FULL_NAME_RG FULL_NAME_ND_RG
 NOTE MODIFY_DATE/;
-
-=head2 parse_file
-
-    parse_file (file => 'ja.txt', data => \@array);
-
-Parse the data in the file specified by C<file> and put the lines into
-the array specified by the C<data> parameter.
-
-    parse_file (file => 'af.txt', callback => \& myroutine);
-
-Parse the data in the file specified by C<file>. As each line of data
-is parsed, call back the code routine specified by C<callback>. The callback is called in the form
-
-    &{callback} (undef, \%line);
-
-Possible options are
-
-=over
-
-=item file
-
-The file name. This must be supplied or the module dies.
-
-=item data
-
-An array reference.
-
-=item callback
-
-A code reference to call back. If parse_file is called as
-
-    parse_file (%inputs);
-
-then the callback is called in the form
-
-    &{$inputs{callback}} ($inputs{callback_data}, \%line);
-
-where C<%line> is a hash containing the parts of the line.
-
-=item callback_data
-
-User-specified data to pass to the callback routine. See callback above.
-
-=back
-
-=cut
 
 sub parse_file
 {
